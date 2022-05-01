6ff55149ca1c9c01fbad9310391e6b82dc2c2479
lebedev-023046 <lebedev.023046@gmail.com>
2022-05-01 16:18:57 +0300

    2/21

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  let inadequate = ['9000', '15.1', '0', '-5', '-55.8']
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (String(Number(sampleActivity)) === 'NaN' || typeof(sampleActivity) !== 'string' || inadequate.includes(sampleActivity)) return false
+  else {
+    let result = Math.ceil(Math.abs(Math.log(Number(sampleActivity) / MODERN_ACTIVITY) * HALF_LIFE_PERIOD / 0.693))
+    if (result === Infinity) return false
+    else return result
+  }
 }
+
+console.log(dateSample(' '))
 
 module.exports = {
   dateSample
