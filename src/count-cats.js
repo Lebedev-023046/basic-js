672c855120b060b633932c93bb6c4091329c93a8
lebedev-023046 <lebedev.023046@gmail.com>
2022-05-01 15:56:52 +0300

    1/21

--- src/count-cats.js
+++ src/count-cats.js
@@ -19,7 +19,7 @@ function countCats(matrix) {
   // remove line with error and write your code here
   let count = 0
   for (let i=0;i<matrix.length;i++) {
-    for (let j=0;i<matrix[i].length;j++) {
+    for (let j=0;j<matrix[i].length;j++) {
       if (matrix[i][j] === '^^') count++
     }
   }
