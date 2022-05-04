321e3ee884d3c5010aeb105ed298290497124010
lebedev-023046 <lebedev.023046@gmail.com>
2022-05-04 12:07:33 +0300

    10/21

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,10 +22,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+domains1 = ['epam.com']
+
+domains2 = ['epam.com', 'info.epam.com']
+
+// const countElem = (elem, domList) => {
+//   let count = 0
+//   domList.flat().forEach(element => {
+//     if (element === elem) count++
+//   })
+//   return count
+// }
+
+let domains = [
+  'code.yandex.ru',
+  'music.yandex.ru',
+  'yandex.ru'
+ ]
+
+function getDNSStats(domains) {
+  // throw new NotImplementedError('Not implemented');
+  let domList = []
+  let domCount = {}
+  domains.forEach(element => {
+    domList.push(element.split(".").reverse())
+  });
+  for (let i=0;i<domList.length;i++) {
+    for (let j=0;j<domList[i].length;j++) {
+      domList[i][j] = `.${domList[i][j]}`
+    }
+  }
+
+  for (let i=0;i<domList.length;i++) {
+
+    //first level
+    if (typeof(domCount[domList[i][0]]) !== 'undefined') domCount[domList[i][0]]++
+    else  domCount[domList[i][0]] = 1
+
+    //second level
+    let secondLevelDmain = domList[i][0] + domList[i][1]
+    if (typeof(domCount[secondLevelDmain]) !== 'undefined') domCount[secondLevelDmain]++
+    else  domCount[secondLevelDmain] = 1
+
+    //third level
+    if (typeof(domList[i][2]) !== 'undefined') {
+      let thirdLevelDmain = domList[i][0] + domList[i][1] + domList[i][2]
+      if (typeof(domCount[thirdLevelDmain]) !== 'undefined') domCount[thirdLevelDmain]++
+      else  domCount[thirdLevelDmain] = 1
+    }
+    }
+  return domCount
 }
+
+console.log(getDNSStats(domains))
 
 module.exports = {
   getDNSStats
